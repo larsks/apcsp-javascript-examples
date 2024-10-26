ea546a1c5c565deddca76ab509ebf5adc0388e57
Lars Kellogg-Stedman <lars@oddbit.com>
2024-10-26 09:19:22 -0400

    Add inventory code

---
+++ inventory/inventory.js
@@ -0,0 +1,19 @@
+require('../codehs');
+
+var ITEMS_IN_INVENTORY = 20;
+
+while (ITEMS_IN_INVENTORY > 0) {
+    println("we have " + ITEMS_IN_INVENTORY + " in inventory ");
+    var items = readInt("how many would you like to buy? ");
+    if (ITEMS_IN_INVENTORY >= items) {
+
+        ITEMS_IN_INVENTORY = ITEMS_IN_INVENTORY - items;
+        println("now we have " + ITEMS_IN_INVENTORY + " left");
+
+    }
+    else {
+        println("There is not enough in inventory for that purchase.");
+    }
+
+}
+println("all out");
