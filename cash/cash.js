12a31750fa0e81f0d14ef9c3b1c42771b4e0fd64
Lars Kellogg-Stedman <lars@oddbit.com>
2024-11-07 22:38:51 -0500

    Fix errors reported by biome

--- cash/cash.js
+++ cash/cash.js
@@ -1,8 +1,8 @@
 require("../codehs");
 
 function how_many_coins(change) {
-	var cents = Math.round(change * 100);
-	var quarters, dimes, nickels;
+	let cents = Math.round(change * 100);
+	let quarters, dimes, nickels;
 
 	quarters = Math.floor(cents / 25);
 	cents = cents - 25 * quarters;
@@ -12,13 +12,12 @@ function how_many_coins(change) {
 	cents = cents - 5 * nickels;
 
 	//println("Q " + quarters + " D " + dimes + " N " + nickels + " P " + cents);
-	var coins = quarters + dimes + nickels + cents;
-	var check = 25 * quarters + 10 * dimes + 5 * nickels + cents;
+	const coins = quarters + dimes + nickels + cents;
 	return coins;
 }
 
 function start() {
-	var tests = [
+	const tests = [
 		[0.01, 1],
 		[0.05, 1],
 		[0.1, 1],
@@ -27,28 +26,21 @@ function start() {
 		[0.99, 9],
 	];
 
-	var failed = 0,
-		passed = 0,
-		total = 0;
+	let failed = 0;
+	let	passed = 0;
+	let total = 0;
 
-	for (var testparms of tests) {
+	for (testparms of tests) {
 		total++;
-		var res = how_many_coins(testparms[0]);
-		if (res != testparms[1]) {
+		const res = how_many_coins(testparms[0]);
+		if (res !== testparms[1]) {
 			failed++;
-			println(
-				"FAILED! given input " +
-					testparms[0] +
-					" expected " +
-					testparms[1] +
-					" got " +
-					res,
-			);
+			println(`FAILED! given input ${testparms[0]} expected ${testparms[1]} got ${res}`);
 		} else {
 			passed++;
 		}
 	}
-	println("total " + total + " passed " + passed + " failed " + failed);
+	println(`total ${total} passed ${passed} failed ${failed}`);
 }
 
 start();
