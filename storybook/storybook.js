8431ca9c1513267e36cc7fa7bc20f1a81b181eef
Lars Kellogg-Stedman <lars@oddbit.com>
2024-11-24 09:34:37 -0500

    Make storybook slightly fancier

--- storybook/storybook.js
+++ storybook/storybook.js
@@ -10,33 +10,49 @@ require("../codehs");
 var hasKey = false;
 
 function getNumber(prompt, min, max) {
-    while (true) {
-        var choice = readInt(prompt);
+  while (true) {
+    const choice = readLine(prompt);
 
-        if (choice == null || (choice >= min && choice <= max)) {
-            return choice;
-        }
+    if (choice == null || choice == 'q')
+      return null;
+
+    const int_choice = parseInt(choice);
+    if (int_choice >= min && int_choice <= max) {
+      return int_choice;
     }
+  }
 }
 
 function start() {
-    var done = false;
+  var done = false;
 
-    while (!done) {
-        println(
-            "you are locked in a room. to get out you can search or you can try to break out"
-        );
-        var choice = getNumber("open door = 1, search = 2 or break out = 3? ", 1, 3);
-        if (choice == null) {
-            return;
-        }
-
-        if (choice == 1) done = opendoor();
-        else if (choice == 2) done = gosearch();
-        else if (choice == 3) done = gobreakout();
+  while (!done) {
+    println(
+      "You are locked in a room. To get out you can search or you can try to break out."
+    );
+    var choice = getNumber("open door = 1, search = 2 or break out = 3? ", 1, 3);
+    if (choice == null) {
+      break;
     }
 
+    switch (choice) {
+      case 1:
+        done = opendoor();
+        break;
+      case 2:
+        done = gosearch();
+        break;
+      case 3:
+        done = gobreakout();
+        break;
+    }
+  }
+
+  if (done) {
     println("You escape!");
+  } else {
+    println("You give up.");
+  }
 }
 
 function opendoor() {
@@ -50,29 +66,29 @@ function opendoor() {
 }
 
 function gosearch() {
-    var holes = getNumber(
-        "how many holes do you want to dig? choose a number between 1-10: ",
-        1,
-        10
-    );
-    if (holes == 2 || holes == 7 || holes == 9) {
-        println("You found a key!");
-        hasKey = true;
-    } else {
+  var holes = getNumber(
+    "How many holes do you want to dig? Choose a number between 1-10: ",
+    1,
+    10
+  );
+  if (holes == 2 || holes == 7 || holes == 9) {
+    println("You found a key!");
+    hasKey = true;
+  } else {
     println("You don't find anything.")
   }
 
-    return false;
+  return false;
 }
 
 function gobreakout() {
-    if (Math.random() > 0.8) {
-        println("You break down the door!");
-        return true;
-    } else {
-        println("you are not strong enough to break out");
-        return false;
-    }
+  if (Math.random() > 0.8) {
+    println("You break down the door!");
+    return true;
+  } else {
+    println("You fail to break down the door.");
+    return false;
+  }
 }
 
 start();
